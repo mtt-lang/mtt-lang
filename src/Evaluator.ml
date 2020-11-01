a347a5bc3b7609d4f1a78d96dc9d4f9841666d63
sloboegen98 <sloboegen98@ya.ru>
2020-11-02 01:19:34 +0300

    evaluator supported

--- src/Evaluator.ml
+++ src/Evaluator.ml
@@ -15,10 +15,27 @@ let rec free_vars_g term =
   | Fun (_i, _t_of_id, body) -> free_vars_g body
   | App (fe, arge) -> Set.union (free_vars_g fe) (free_vars_g arge)
   | Box e -> free_vars_g e
-  | Let _ -> assert false
+  | Let (_i, _bounded_e, body) -> free_vars_g body
   | Letbox (i, boxed_e, body) ->
       Set.union (free_vars_g boxed_e)
         (Set.diff (free_vars_g body) (Set.singleton (module Id.M) i))
+
+let rec free_vars_r term =
+  let open Expr in
+  match term with
+  | Unit -> Set.empty (module Id.R)
+  | Pair (e1, e2) -> Set.union (free_vars_r e1) (free_vars_r e2)
+  | Fst pe | Snd pe -> free_vars_r pe
+  | VarL i -> Set.singleton (module Id.R) i
+  | VarG _i -> Set.empty (module Id.R)
+  | Fun (i, _t_of_id, body) ->
+      Set.diff (free_vars_r body) (Set.singleton (module Id.R) i)
+  | App (fe, arge) -> Set.union (free_vars_r fe) (free_vars_r arge)
+  | Box _e -> Set.empty (module Id.R)
+  | Let (i, bounded_e, body) ->
+      Set.union (free_vars_r bounded_e)
+        (Set.diff (free_vars_r body) (Set.singleton (module Id.R) i))
+  | Letbox (_i, _boxed_e, body) -> free_vars_r body
 
 let refresh_g idg fvs =
   let rec loop (idg : Id.M.t) =
@@ -26,6 +43,12 @@ let refresh_g idg fvs =
     (* it's fresh enough already :) *)
   in
   if Set.mem fvs idg then Some (loop idg) else None
+
+let refresh_r idr fvs =
+  let rec loop (idr : Id.R.t) =
+    if Set.mem fvs idr then loop (Id.R.mk (Id.R.to_string idr ^ "'")) else idr
+  in
+  if Set.mem fvs idr then Some (loop idr) else None
 
 (* modal (modal) substitution *)
 let rec subst_m term idg body =
@@ -40,7 +63,7 @@ let rec subst_m term idg body =
   | Fun (idl, t_of_id, body) -> Fun (idl, t_of_id, subst_m term idg body)
   | App (fe, arge) -> App (subst_m term idg fe, subst_m term idg arge)
   | Box e -> Box (subst_m term idg e)
-  | Let _ -> assert false
+  | Let (_i, _bouned_e, body) -> body
   | Letbox (i, boxed_e, body) -> (
       if [%equal: Id.M.t] idg i then Letbox (i, subst_m term idg boxed_e, body)
       else
@@ -54,6 +77,32 @@ let rec subst_m term idg body =
         | None ->
             (* no need to rename the bound var *)
             Letbox (i, subst_m term idg boxed_e, subst_m term idg body) )
+
+(* regular substitution  *)
+let rec subst_r term idr body =
+  let open Expr in
+  match body with
+  | Unit -> body
+  | Pair (e1, e2) -> Pair (subst_r term idr e1, subst_r term idr e2)
+  | Fst pe -> Fst (subst_r term idr pe)
+  | Snd pe -> Snd (subst_r term idr pe)
+  | VarL i -> if [%equal: Id.R.t] idr i then term else body
+  | VarG _i -> body
+  | Fun (idl, t_of_id, body) -> Fun (idl, t_of_id, subst_r term idr body)
+  | App (fe, arge) -> App (subst_r term idr fe, subst_r term idr arge)
+  | Box _e -> body
+  | Let (i, bounded_e, body) -> (
+      if [%equal: Id.R.t] idr i then Let (i, subst_r term idr bounded_e, body)
+      else
+        match refresh_r i (free_vars_r term) with
+        | Some new_i ->
+            let body_with_renamed_bound_var = subst_r (VarL new_i) i body in
+            Let
+              ( new_i,
+                subst_r term idr bounded_e,
+                subst_r term idr body_with_renamed_bound_var )
+        | None -> Let (i, subst_r term idr bounded_e, subst_r term idr body) )
+  | Letbox (_i, _boxed_e, body) -> body
 
 let rec eval_open gamma expr =
   let open Expr in
@@ -84,7 +133,7 @@ let rec eval_open gamma expr =
           eval_open (Env.extend_l c_gamma idl argv) body
       | _ -> Result.fail "Trying to apply an argument to a non-function" )
   | Box e -> return @@ Val.Box e
-  | Let _ -> assert false
+  | Let (idr, bounded_e, body) -> eval_open gamma (subst_r bounded_e idr body)
   | Letbox (idg, boxed_e, body) -> (
       let%bind boxed_v = eval_open gamma boxed_e in
       match boxed_v with
--- src/Evaluator.mli
+++ src/Evaluator.mli
@@ -7,15 +7,28 @@ type error = string
 val free_vars_g : Expr.t -> (Id.M.t, Id.M.comparator_witness) Set.t
 (** Compute the set of free modal variables in a term *)
 
+val free_vars_r : Expr.t -> (Id.R.t, Id.R.comparator_witness) Set.t
+(** Compute the set of free regular variables in a term *)
+
 val refresh_g :
   Id.M.t -> (Id.M.t, Id.M.comparator_witness) Set.t -> Id.M.t option
 (** Given a modal identifier and a set of free modal variables,
     come up with a new name that won't capture any of the free modal variables.
     Returns [None] if no refreshment is needed and [Some new_name] otherwise *)
 
+val refresh_r :
+  Id.R.t -> (Id.R.t, Id.R.comparator_witness) Set.t -> Id.R.t option
+(** Given a regular identifier and a set of free regular variables,
+    come up with a new name that won't capture any of the free regular variables.
+    Returns [None] if no refreshment is needed and [Some new_name] otherwise *)
+
 val subst_m : Expr.t -> Id.M.t -> Expr.t -> Expr.t
 (** Capture-avoiding modal substitution: "[term/idg]body",
     i.e. substitute [term] for free variable [idg] in [body] *)
+
+val subst_r : Expr.t -> Id.R.t -> Expr.t -> Expr.t
+(** Capture-avoiding regular substitution: "[term/idr]body",
+    i.e. substitute [term] for free variable [idr] in [body] *)
 
 val eval_open : Val.t Env.l -> Expr.t -> (Val.t, error) Result.t
 (** Evaluate a possibly open term in a regular context *)
