c3b7fcf058ca02043976147c4838554690ccb51f
Anton Trunov <anton.a.trunov@gmail.com>
2020-12-01 17:48:35 +0300

    refactor: remove code duplication for environments (#42)

--- src/Evaluator.mli
+++ src/Evaluator.mli
@@ -17,7 +17,7 @@ val subst_m : Expr.t -> Id.M.t -> Expr.t -> Expr.t
 (** Capture-avoiding modal substitution: "[term/idg]body",
     i.e. substitute [term] for free variable [idg] in [body] *)
 
-val eval_open : Val.t Env.r -> Expr.t -> (Val.t, error) Result.t
+val eval_open : Val.t Env.R.t -> Expr.t -> (Val.t, error) Result.t
 (** Evaluate a possibly open term in a regular context *)
 
 val eval : Expr.t -> (Val.t, error) Result.t
