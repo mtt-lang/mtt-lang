ce7568db2bd6b662bf09b4a53a03de385ab6a3d7
sloboegen98 <sloboegen98@ya.ru>
2021-05-03 20:16:08 +0300

    bench: add simple benchmark for self-application

---
+++ bench/Generator.ml
@@ -0,0 +1,36 @@
+open Base
+open Mtt
+open Mtt.Ast
+
+let rec gen_self_app (n : int) : Expr.t' Location.located =
+  (* types are wrong. It doesn't matter *)
+  let termId =
+    Expr.func (Id.R.mk "x") (Type.Base { idt = "A" }) (Expr.var_r @@ Id.R.mk "x")
+  in
+  if phys_equal n 0 then Expr.nat @@ Nat.of_int 42
+  else Expr.app termId (gen_self_app @@ (n - 1))
+
+let benchmark name exec =
+  let tstart = Unix.gettimeofday () in
+  for _ = 1 to 100000 do
+    (* n \in [1000, 2000] *)
+    let n = 1000 + Random.int 1000 in
+    let term = gen_self_app n in
+    let _ = exec term in
+    ()
+  done;
+  let tend = Unix.gettimeofday () in
+  Stdio.print_endline
+    (Printf.sprintf "%12s: %.2f secs%!" name (tend -. tstart))
+
+let _ =
+  benchmark "Evaluator" Evaluator.eval;
+  benchmark "Compiler" Compiler.compile
+
+(* 
+
+On my laptop:
+   Evaluator: 16.03 secs
+   Compiler: 18.11 secs
+
+*)
