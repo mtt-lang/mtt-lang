5af1cf92e68da4191e2bd41eb1b8d31f65d9ea00
sloboegen98 <sloboegen98@ya.ru>
2021-05-09 19:08:21 +0300

    test: add bench for power function

--- bench/Generator.ml
+++ bench/Generator.ml
@@ -1,4 +1,5 @@
 open Base
+open Base.Result.Let_syntax
 open Mtt
 open Mtt.Ast
 
@@ -14,22 +15,60 @@ let rec gen_fib (n : int) : Expr.t' Location.located =
   if n <= 2 then Expr.nat @@ Nat.of_int n
   else Expr.binop Expr.Add (gen_fib (n - 1)) (gen_fib (n - 2))
 
-let benchmark name exec =
-  let x = gen_fib 30 in
+let power_slow_100 =
+  let power_slow_text_100 =
+    "fun x: Nat.\n\
+    \  let power = fix (f : Nat -> Nat -> Nat) a : Nat.\n\
+     λb : Nat.\n\
+    \ match b with\n\
+    \ | zero => 1\n\
+    \ | succ sb => ((f a) sb) * a\n\
+     end in (power x) 100"
+  in
+  Util.parse_from_e Term (ParserInterface.String power_slow_text_100)
+
+let power_fast_100 =
+  let power_fast_text_100 =
+    "let pow_n = fix (f: (Nat -> [](Nat -> Nat))) n : Nat.\n\
+    \    match n with\n\
+    \    | zero => box (λb : Nat. 1)\n\
+    \    | succ pn => letbox pred_pow' = f pn in box (λb : Nat . b * \
+     (pred_pow' b))\n\
+    \    end\n\
+    \  in letbox pow' = pow_n 100 in pow'"
+  in
+  Util.parse_from_e Term (ParserInterface.String power_fast_text_100)
+
+let benchmark name ver exec =
+  let%bind pwr_ver = ver in
   let tstart = Unix.gettimeofday () in
-  for _ = 1 to 100 do
-    (* let n = 15 + Random.int 5 in *)
-    (* let term = gen_fib n in *)
-    let term = x in
-    let _ = exec term in
+  for _ = 1 to 1000000 do
+    (* 1 <= b <= 5 *)
+    let b = 1 + Random.int 5 in
+    let _ = exec @@ Expr.app pwr_ver (Expr.nat (Nat.of_int b)) in
     ()
   done;
   let tend = Unix.gettimeofday () in
-  Stdio.print_endline (Printf.sprintf "%12s: %.2f secs%!" name (tend -. tstart))
+  Stdio.print_endline (Printf.sprintf "%12s: %.2f secs%!" name (tend -. tstart));
+  Ok ()
 
 let _ =
-  benchmark "Evaluator" Evaluator.eval;
-  benchmark "Compiler" Compiler.compile
+  let _ = benchmark "Evaluator + slow" power_slow_100 Evaluator.eval in
+  let _ = benchmark "Compiler + slow" power_slow_100 Compiler.compile in
+  let _ = benchmark "Evaluator + fast" power_fast_100 Evaluator.eval in
+  let _ = benchmark "Compiler + fast" power_fast_100 Compiler.compile in
+  Ok ()
+
+(*
+  power function, on my laptop
+  1000000 iterations
+  n^100, where 1 <= n <= 5
+
+  Evaluator + slow: 30.60 secs
+  Compiler + slow: 0.42 secs
+  Evaluator + fast: 27.13 secs
+  Compiler + fast: 0.36 secs  
+*)
 
 (* 
 
