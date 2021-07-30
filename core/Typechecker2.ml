c385ab3fb7c6a314b4ac36afe2c3c52c7b293159
sloboegen98 <sloboegen98@ya.ru>
2021-07-30 18:02:25 +0300

    fix: add missed braces

--- core/Typechecker2.ml
+++ core/Typechecker2.ml
@@ -6,7 +6,7 @@ let rec translate_type ty =
   | Unit -> "unit"
   | Base { idt } -> "(base \"" ^ idt ^ "\")"
   | Prod { ty1; ty2 } ->
-      "prod " ^ "(" ^ translate_type ty1 ^ ") (" ^ translate_type ty2 ^ ")"
+      "product " ^ "(" ^ translate_type ty1 ^ ") (" ^ translate_type ty2 ^ ")"
   | Arr { dom; cod } ->
       "arr " ^ "(" ^ translate_type dom ^ ") (" ^ translate_type cod ^ ")"
   | Box _ -> failwith "Box type isn't implemented yet"
@@ -22,9 +22,9 @@ let rec translate Location.{ data = expr; _ } =
       ^ ")"
   | App { fe; arge } ->
       "app (" ^ translate fe ^ ") " ^ "(" ^ translate arge ^ ")"
-  | Pair { e1; e2 } -> "pair " ^ translate e1 ^ " " ^ translate e2
-  | Fst { e } -> "fst " ^ translate e
-  | Snd { e } -> "snd " ^ translate e
+  | Pair { e1; e2 } -> "pair (" ^ translate e1 ^ ") (" ^ translate e2 ^ ")"
+  | Fst { e } -> "fst (" ^ translate e ^ ")"
+  | Snd { e } -> "snd (" ^ translate e ^ ")"
   | Let { idr; bound; body } ->
       "let (" ^ translate bound ^ ") " ^ "(" ^ Id.R.to_string idr ^ "\\"
       ^ translate body ^ ")"
