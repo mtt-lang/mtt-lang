d92c8c6e359939a78b7711fb67d2e22a1dbcca8d
sloboegen98 <sloboegen98@ya.ru>
2021-07-30 16:00:12 +0300

    fix: don't crash

--- core/LambdaTypechecker.ml
+++ core/LambdaTypechecker.ml
@@ -15,6 +15,9 @@ let rec prolog2str expr =
   | API.RawData.Const cons_code -> (
       match cons_code with -475 -> "()" | _ -> string_of_int cons_code)
   | API.RawData.Lam lam -> "(" ^ prolog2str lam ^ ")"
+  (* now cons is only `arr` and `size t2 = 1`  *)
+  | API.RawData.App (_cons, t1, t2) ->
+      prolog2str t1 ^ " -> " ^ String.concat " " @@ List.map prolog2str t2
   | API.RawData.UnifVar (fd, xs) ->
       API.FlexibleData.Elpi.show fd
       ^ String.concat " " @@ List.map prolog2str xs
