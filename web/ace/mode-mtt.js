1de89a8971d0e6e37fd177f5811ab5fd6feba94c
SmnTin <smn.pankv@gmail.com>
2022-05-21 22:27:15 +0300

    Add new tokens to syntax highlighting

--- web/ace/mode-mtt.js
+++ web/ace/mode-mtt.js
@@ -9,11 +9,11 @@ ace.define("ace/mode/mtt_highlighting_rules", ["require", "exports", "module", "
             "start": [
                 {
                     token: "keyword",
-                    regex: "let|letbox|in|fst|snd|box|fun|type|of"
+                    regex: "let|letbox|in|fst|snd|box|fun|type|of|match|with|end"
                 },
                 {
                     token: "keyword.operator",
-                    regex: "\\[\\]|=>|->|\\*|<|>|λ"
+                    regex: "\\[\\]|=>|->|\\*|<|>|λ|=|,|\\||;"
                 }
             ]
         
